18d2ecfee3b32db3361310473e06626a8c4caa5b
ikeh045 <njkampret@gmail.com>
2020-12-13 02:05:23 +0700

    bring up rr for kenzo

--- rr_kenzo.mk
+++ rr_kenzo.mk
@@ -20,15 +20,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kipper device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
 
-# Inherit some common BlissOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common rr stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := havoc_kenzo
+PRODUCT_NAME := rr_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
@@ -53,10 +53,6 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
 USE_CCACHE=0
-
-# Havoc build type
-HAVOC_BUILD_TYPE := Official
-ro.havoc.maintainer := Magicxavi
 
 PRODUCT_PACKAGES += \
     AdvancedControls \
