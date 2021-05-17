b027754bcd3a1ae8d8c2bcfc8b7f4b7ee5c5a2b7
aaryanmarriwala <67773762+aaryanmarriwala@users.noreply.github.com>
2021-05-17 16:44:49 +0530

    Update rr_kenzo.mk

--- rr_kenzo.mk
+++ rr_kenzo.mk
@@ -21,19 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
 
 # Inherit some common rr stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := rr_kenzo
+PRODUCT_NAME := aosp_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
-
-RR_BUILDTYPE=Official
 
 IS_PHONE := true
 TARGET_INCLUDE_STOCK_ARCORE := true
