25306ca3f7e15148b7aab0fcf4567a9f36718065
aaryanmarriwala <67773762+aaryanmarriwala@users.noreply.github.com>
2021-05-19 12:33:08 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,8 +231,9 @@ TARGET_LD_SHIM_LIBS := \
 
 # SELinux
 BOARD_SEPOLICY_VERS := 29.0
-include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
